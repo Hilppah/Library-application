ccf61d2772d72b010292b16cd4c79f475aeef2e3
Hilppah <hilppa.huhtanen@gmail.com>
2025-01-13 20:13:14 +0200

    Initial commit

---
+++ Backend/App/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
