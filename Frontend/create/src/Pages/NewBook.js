31d0b21fffb9806ae1022af1dde95333b40e6980
Hilppah <hilppa.huhtanen@gmail.com>
2025-02-13 20:52:21 +0200

    Update frontend

--- Frontend/create/src/Pages/NewBook.js
+++ Frontend/create/src/Pages/NewBook.js
@@ -1,8 +1,26 @@
 import { Link } from "react-router-dom";
-import "../Styling/ReturnBook.css";
-import { Navbar } from "react-bootstrap";
+import "../Styling/NewBook.css";
+import { Container, Navbar } from "react-bootstrap";
+import { useState } from "react";
+
+const genres = [
+  "Children's",
+  "Fantasy",
+  "Historical fiction",
+  "Horror",
+  "Non-fiction",
+  "Romace",
+  "Sci-fi",
+  "Thriller",
+  "Young adult",
+];
 
 const NewBookPage = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [year, setYear] = useState("");
+  const [genre, setGenre] = useState(genres[0]);
+
   return (
     <div className="Home">
       <Navbar className="Navbar">
@@ -17,21 +35,21 @@ const NewBookPage = () => {
               <span className="textButton">Books</span>
             </button>
           </Link>
-          <Link to="/new-user">
-            <button className="buttonReturn">
-              <span className="textButton">Save a new user</span>
-            </button>
-          </Link>
           <Link to="/users">
             <button className="buttonUsers">
               <span className="textButton">Users</span>
+            </button>
+          </Link>
+          <Link to="/new-user">
+            <button className="buttonReturn">
+              <span className="textButton">Save a new user</span>
             </button>
           </Link>
         </Navbar.Brand>
       </Navbar>
       <div className="containerNewUsePage">
         <div className="containerLight">
-          <h3 className="new-userText">Return a book</h3>
+          <h3 className="new-userText">Add a new book</h3>
           <div className="inputGroup">
             <label htmlFor="bookName" className="Text">
               Book title:
@@ -53,16 +71,31 @@ const NewBookPage = () => {
               name="author"
               placeholder="Enter author's name"
             />
-            <label htmlFor="customerNumber" className="Text">
-              Customer's name:
+            <label htmlFor="publicationYear" className="Text">
+              Publication year:
             </label>
             <input
               className="inputUser"
               type="text"
-              id="phone"
-              name="customerNumber"
-              placeholder="Enter customer's phone number"
+              id="year"
+              name="publicationYear"
+              placeholder="Enter the publication year"
             />
+            <div>
+              <label className="Text">Genres:</label>
+              <Container className="containerGenres">
+                <div className="inputGroup">
+                  <div className="genreCheckboxes">
+                    {genres.map((genre) => (
+                      <label key={genre} className="checkboxLabel">
+                        <input type="checkbox" value={genre} />
+                        {genre}
+                      </label>
+                    ))}
+                  </div>
+                </div>
+              </Container>
+            </div>
           </div>
           <div className="inputGroup">
             <button className="buttonSave">
